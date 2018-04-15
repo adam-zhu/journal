e479e9e4e6dc072c5bcee40d1d4dcae1b61a38c2
Adam Zhu <adamzhu@Threatcares-MacBook-Pro.local>
2018-04-14 19:53:23 -0500

    remove dead code:

--- src/App.js
+++ src/App.js
@@ -199,19 +199,3 @@ class App extends Component {
 }
 
 export default App;
-
-const isUserEqual = (googleUser, firebaseUser) => {
-  if (firebaseUser) {
-    var providerData = firebaseUser.providerData;
-    for (var i = 0; i < providerData.length; i++) {
-      if (
-        providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-        providerData[i].uid === googleUser.getBasicProfile().getId()
-      ) {
-        // We don't need to reauth the Firebase connection.
-        return true;
-      }
-    }
-  }
-  return false;
-};
